be0f43cc10dfef57d9614b7a8edf6cbce4f6bc6c
Brandon Huffman <bt_huffman@msn.com>
2019-01-15 08:44:51 -0700

    Created two mathematical functions and one concatenating function.

---
+++ script.js
@@ -0,0 +1,18 @@
+function addNumbers (parameter1,parameter2) {
+	let added = parameter1 + parameter2;
+	document.write(added + "<br>");
+}
+
+addNumbers(1,2);
+
+function concatenate(starship,spacestation) {
+	document.write(starship + " to space station " + spacestation + " one come in please.");
+}
+concatenate("Enterprise","Regula");
+
+function subtractNumbers (parameter1,parameter2) {
+	let subtracted = parameter1 - parameter2;
+	alert(subtracted);
+}
+
+subtractNumbers(1,2);
