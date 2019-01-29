61f43e9914e3c6c47e20e66b841bdc692c4fcd6a
Brandon Huffman <bt_huffman@msn.com>
2019-01-29 09:14:37 -0700

    Attempted snap challenge, added george's method for completion.

---
+++ 1-29-19 anonymous.js
@@ -0,0 +1,11 @@
+/*var myArray =[4,6,5,9,8];
+
+var result = myArray.map(function(value,index,array){
+	return value * index;
+});
+document.write(result);
+*/
+let user = new Map();
+ users.set('address','many things');
+ let a = users.get('address');
+ console.log(a);
